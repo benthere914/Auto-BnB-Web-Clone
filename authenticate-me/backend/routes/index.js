d2468fdbb1ad9707394d8fe55335ca6298214176
Benjamin Rose <benthere914@gmail.com>
2021-10-07 14:44:47 -0500

    frontend init

--- authenticate-me/backend/routes/index.js
+++ authenticate-me/backend/routes/index.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const router = express.Router();
-const apiRouter = require('./api')
+const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
-router.get('/hello/world', function(req, res) {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World!');
-  console.log('test')
-});
 
-console.log(123)
+if (process.env.NODE_ENV === 'production') {
+	const path = require('path');
+	router.get('/', (req, res) => {
+		res.cookie('XSRF-TOKEN', req.csrfToken());
+		return res.sendFile(
+			path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+		);
+	});
+	router.use(express.static(path.resolve('../frontend/build')));
+	router.get(/^(?!\/?api).*/, (req, res) => {
+		res.cookie('XSRF-TOKEN', req.csrfToken());
+		return res.sendFile(
+			path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+		);
+	});
+}
+
+if (process.env.NODE_ENV !== 'production') {
+	router.get('/api/csrf/restore', (req, res) => {
+		res.cookie('XSRF-TOKEN', req.csrfToken());
+		return res.json({});
+	});
+}
+
 module.exports = router;
