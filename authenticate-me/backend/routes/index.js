60f28edb3869e43635408bb95a2d79bcb0262fe4
Benjamin Rose <benthere914@gmail.com>
2021-10-07 10:39:53 -0500

    init

---
+++ authenticate-me/backend/routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/hello/world', function(req, res) {
+  res.cookie('XSRF-TOKEN', req.csrfToken());
+  res.send('Hello World!');
+  console.log('test')
+});
+
+console.log(123)
+module.exports = router;
