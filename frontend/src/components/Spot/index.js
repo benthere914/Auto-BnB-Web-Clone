a74508ce06ea055670e9f17f591a21b72473be0e
Benjamin Rose <benthere914@gmail.com>
2021-10-11 23:10:52 -0500

    shutting down for the night

--- frontend/src/components/Spot/index.js
+++ frontend/src/components/Spot/index.js
@@ -1,24 +1,32 @@
-import { useParams } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import * as spotActions from '../../store/spot'
+import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import * as spotActions from '../../store/spot';
 export const Spot = () => {
-    let dispatch = useDispatch();
-    const {spotId} = useParams();
-    useEffect(() => {dispatch(spotActions.loadSpot(+spotId))}, [dispatch])
-    const { data } = useSelector(state => state.spot);
-    return (
-        <>
-        <div>
-            <h1>{data?.title} by {data?.author.username}</h1>
-        </div>
-        <div>
-            {data?.images.map(e => (<img src={e.url} alt={e.alt}></img>))}
-        </div>
-        <div>
-            <h3>{`Wonderful ${data?.type} hosted by ${data?.author.username}`}</h3>
-        </div>
-        </>
-    )
-}
+	let dispatch = useDispatch();
+	const { spotId } = useParams();
+	useEffect(() => {
+		dispatch(spotActions.loadSpot(+spotId));
+	}, [dispatch]);
+	const { data } = useSelector((state) => state.spot);
+	return (
+		<>
+			<div>
+				<div>
+					<h1>
+						{data?.title} by {data?.author.username}
+					</h1>
+				</div>
+				<div>
+					{data?.images.map((e) => (
+						<img src={e.url} alt={e.alt}></img>
+					))}
+				</div>
+				<div>
+					<h3>{`Wonderful ${data?.type} hosted by ${data?.author.username}`}</h3>
+				</div>
+			</div>
+		</>
+	);
+};
